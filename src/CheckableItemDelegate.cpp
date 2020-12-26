65b1db9bb1c653ff1435b1d91011c68c8f139158
Neptilo <neptilo@gmail.com>
2020-12-26 22:55:53 +0100

    Simplify checkable item delegate

--- src/CheckableItemDelegate.cpp
+++ src/CheckableItemDelegate.cpp
@@ -16,22 +16,17 @@ bool CheckableItemDelegate::editorEvent(
         const QModelIndex &index)
 {
     Q_UNUSED(option);
-    if (event->type() == QEvent::MouseButtonRelease) {
-        if (enabled) {
-            if (index.flags() & Qt::ItemIsUserCheckable) {
-                QVariant value = index.data(Qt::CheckStateRole);
-                Qt::CheckState state =
-                        (static_cast<Qt::CheckState>(value.toInt()) == Qt::Checked ?
-                             Qt::Unchecked : Qt::Checked);
-                return model->setData(index, state, Qt::CheckStateRole);
-            } else {
-                return false;
-            }
-        } else {
-            set_enabled(true);
-            return false;
-        }
+    if (event->type() != QEvent::MouseButtonRelease)
+        return false;
+
+    if (enabled) {
+        QVariant value = index.data(Qt::CheckStateRole);
+        Qt::CheckState state =
+                (static_cast<Qt::CheckState>(value.toInt()) == Qt::Checked ?
+                     Qt::Unchecked : Qt::Checked);
+        return model->setData(index, state, Qt::CheckStateRole);
     } else {
+        set_enabled(true);
         return false;
     }
 }
